79a627f1aa6cb0f7ae1da9e231911b0f03fc2c7a
Szboy <Szboy@users.noreply.github.com>
2020-12-08 13:22:15 -0500

    Merge updatedHeatmap

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,25 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Container, Col, Image, Row, Jumbotron } from 'react-bootstrap';
+import { Button, Container, Col, Row, Jumbotron } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/App.css';
 
 export class Main extends Component {
-    geocodeFunction() {
-        var location = '805 7th street south Safety Harbor FL' 
-        Axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
-            params: {
-                address: location,
-                key: 'AIzaSyB5eyGVQrjug2rhMnXHDpwdrsdSESUN9Z4'
-            }
-        })
-        .then(function(response) {
-            var lati = (response.data.results[0].geometry.location.lat);
-            var longi = (response.data.results[0].geometry.location.lng);
-            console.log("The coordinates are Lat:" + lati + " and Long: " + longi );    
-        });
-    };
-
     render() {
         return (
             <Container>
@@ -51,7 +36,7 @@ export class Main extends Component {
                 <hr/>
                 <br/>
                 <h5>Positive Case Heat Map</h5>
-                <iframe styles="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);" width="1080" height="720" src="https://charts.mongodb.com/charts-project-0-pmwni/embed/charts?id=57f65d96-ea7f-48db-81fb-b25f37fbbf4e&theme=light"></iframe>
+                <iframe title="Heatmap" styles="background: #FFFFFF;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);" width="1080" height="720" src="https://charts.mongodb.com/charts-project-0-pmwni/embed/charts?id=57f65d96-ea7f-48db-81fb-b25f37fbbf4e&theme=light"></iframe>
             </Container>
         );
     }
