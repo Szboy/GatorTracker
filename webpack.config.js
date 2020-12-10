1a1defb96810e91dbaeefd8cb9e573b590a63552
Szboy <Szboy@users.noreply.github.com>
2020-12-09 19:03:02 -0500

    Remove parsed

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack');
 const dotenv = require('dotenv');
 
 module.exports = () => {
-    const env = dotenv.config().parsed;
+    const env = dotenv.config();
 
     // reduce it to a nice object, the same as before
     const envKeys = Object.keys(env).reduce((prev, next) => {
